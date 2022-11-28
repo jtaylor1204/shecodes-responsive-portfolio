747e618fc17ba491fb266dcd90b30c3ba0a6cf56
jtaylor1204 <jaidataylor4@gmail.com>
2022-11-28 12:52:40 -0500

    updated work page

--- js/index.js
+++
@@ -1,6 +0,0 @@
-// Animated Emojis
-// const myCarouselElement = document.querySelector('#myCarousel')
-// const carousel = new bootstrap.Carousel(myCarouselElement, {
-//   interval: 2000,
-//   wrap: false
-// })
