08be97e98411bbdbe306498723757c149aa0cbdc
Jaida Taylor <jaidataylor4@gmail.com>
2023-06-23 00:34:48 -0400

    updated work slideshow

--- js/index.js
+++ js/index.js
@@ -1,16 +1,9 @@
+$(document).ready(function () {
+    let image =  $('.sample-img');
+   image.hover(function (){
+  
+        $('#workCarousel').toggleClass('stop');
+        
 
-
-// flexFont = function () {
-//   var divs = document.getElementsByClassName("flexFont");
-//   for(var i = 0; i < divs.length; i++) {
-//       var relFontsize = divs[i].offsetWidth*0.03;
-//       divs[i].style.fontSize = relFontsize+'px';
-//   }
-// };
-
-// window.onload = function(event) {
-//   flexFont();
-// };
-// window.onresize = function(event) {
-//   flexFont();
-// };
+    });
+});
